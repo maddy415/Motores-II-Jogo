0602da3406470e6285c830106c4ffcf3e463cff8
maddy <luis.maia@escolar.ifrn.edu.br>
2024-11-08 14:12:21 -0300

    Pulo + animação

--- Ultrakill 2/Assets/Scripts/Player.cs
+++ Ultrakill 2/Assets/Scripts/Player.cs
@@ -1,20 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
     private Animator anim;
     public float speed;
+    public float jumpForce;
+    public bool isJumping = false;
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -39,11 +45,25 @@ public class Player : MonoBehaviour
             anim.SetBool("Walk", false);
             Debug.Log("soltou");
         }
-        
     }
+
 
     void Jump()
     {
+        if ((Input.GetKeyDown(KeyCode.Space)) && (isJumping!=true))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+            anim.SetBool("Jump", true);
+        }
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = false;
+            anim.SetBool("Jump", false);
+        }
     }
 }
