aba5b6375dc91cb3a925854e9aeab7883215274d
maddy <luis.maia@escolar.ifrn.edu.br>
2024-11-28 14:32:23 -0300

    Melhoria da IA do inimigo

--- Ultrakill 2/Assets/Scripts/EnemyAI.cs
+++ Ultrakill 2/Assets/Scripts/EnemyAI.cs
@@ -6,19 +6,43 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
     private Rigidbody2D rb;
-    private Vector3 PlayerPos;
     public float walkSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPos = player.transform.position;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(PlayerPos.x, 0f, 0f) * walkSpeed;
+        if (transform.position.x > player.transform.position.x)
+        {
+            transform.position += Vector3.left * walkSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            Debug.Log("x maior");
+        }
+        
+        if (transform.position.x < player.transform.position.x)
+        {
+            transform.position += Vector3.right * walkSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            Debug.Log("x menor");
+        }
+        
+        
+        
+        /*Vector3 scale = transform.localScale;
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1;
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale; */
     }
 }
