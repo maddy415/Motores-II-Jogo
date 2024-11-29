01700720564442c2b81f6c03930743a18e5d5d78
maddy <luis.maia@escolar.ifrn.edu.br>
2024-11-29 00:23:34 -0300

    Inimigo morre

--- Ultrakill 2/Assets/Scripts/EnemyAI.cs
+++ Ultrakill 2/Assets/Scripts/EnemyAI.cs
@@ -4,45 +4,32 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public GameObject player;
-    private Rigidbody2D rb;
+    public Transform playerTransform;
+    
     public float walkSpeed;
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        if (transform.position.x > player.transform.position.x)
+        if (transform.position.x > playerTransform.transform.position.x)
         {
             transform.position += Vector3.left * walkSpeed * Time.deltaTime;
             transform.eulerAngles = new Vector3(0, 180, 0);
-            Debug.Log("x maior");
+            
         }
         
-        if (transform.position.x < player.transform.position.x)
+        if (transform.position.x < playerTransform.transform.position.x)
         {
             transform.position += Vector3.right * walkSpeed * Time.deltaTime;
             transform.eulerAngles = new Vector3(0, 0, 0);
-            Debug.Log("x menor");
+            
         }
         
-        
-        
-        /*Vector3 scale = transform.localScale;
-        if (player.transform.position.x > transform.position.x)
-        {
-            scale.x = Mathf.Abs(scale.x) * -1;
-        }
-        else
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-
-        transform.localScale = scale; */
     }
 }
