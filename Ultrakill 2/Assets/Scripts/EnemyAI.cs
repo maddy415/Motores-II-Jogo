a36bd6e10d2e833192341ff03b9667c130d2500e
maddy <luis.maia@escolar.ifrn.edu.br>
2024-12-12 10:11:57 -0300

    a

--- Ultrakill 2/Assets/Scripts/EnemyAI.cs
+++ Ultrakill 2/Assets/Scripts/EnemyAI.cs
@@ -6,19 +6,30 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
     private Rigidbody2D rb;
-    private Vector3 PlayerPos;
-    public float walkSpeed;
+    public float walkSpeed; 
+    public Transform playerTransform;
     
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPos = player.transform.position;
-        rb = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3(PlayerPos.x, 0f, 0f) * walkSpeed;
+        if (transform.position.x > playerTransform.transform.position.x)
+        {
+            transform.position += Vector3.left * walkSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        if (transform.position.x < playerTransform.transform.position.x)
+        {
+            transform.position += Vector3.right * walkSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        
+        
     }
 }
